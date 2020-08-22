f6a549bf7a9eb5ea86e57049dd1fc0ce27c1b436
Victoria Rogozhkina <v.rogozhkina@gmail.com>
2020-08-21 22:26:13 -0700

    Updates

---
+++ precache-manifest.d2b137a1acf62f962c427e12407a1a41.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a4f2b2f733217bbc338b77137397af9d",
+    "url": "/todoRedux/index.html"
+  },
+  {
+    "revision": "46b62cafc2f1ee26bf63",
+    "url": "/todoRedux/static/css/main.b168c1f4.chunk.css"
+  },
+  {
+    "revision": "9dcfafa55fb391887807",
+    "url": "/todoRedux/static/js/2.d8565208.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/todoRedux/static/js/2.d8565208.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "46b62cafc2f1ee26bf63",
+    "url": "/todoRedux/static/js/main.fafc4dc7.chunk.js"
+  },
+  {
+    "revision": "a3a5b876ba1c59f47548",
+    "url": "/todoRedux/static/js/runtime-main.838d46ea.js"
+  }
+]);
